ac7a3361fbebf44e050eea45d3d4ae1500c770d4
Administrator <Administrator@CRG-PC-028.stairway.local>
2020-11-11 08:48:48 +0700

    tambah rtspp camera

--- rtsp_stream.py
+++ rtsp_stream.py
@@ -2,7 +2,10 @@ import cv2
 #rtsp using camon live streaming android
 #video = cv2.VideoCapture("rtsp://10.10.1.240:8080/video/h264")
 #rtsp using hikvision ds2cd1021-i 
-video = cv2.VideoCapture("rtsp://view:futami2020@10.10.1.21:554/Streaming/Channels/102")
+#video = cv2.VideoCapture("rtsp://view:futami2020@10.10.1.21:554/Streaming/Channels/102")
+
+#rtsp using camera v380 pro 160k
+video = cv2.VideoCapture("rtsp://10.10.1.216/live/ch00_1")
 
 while True:
     ret, frame = video.read()
